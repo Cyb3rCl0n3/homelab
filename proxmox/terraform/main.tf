01fc4f53c440118c0534d67cebf5276f75c54b8a
Cyb3rCl0n3 <96691951+Cyb3rCl0n3@users.noreply.github.com>
2026-07-07 13:39:03 +0000

    test workflow

--- proxmox/terraform/main.tf
+++ proxmox/terraform/main.tf
@@ -12,7 +12,7 @@ module "test_vm" {
   gateway        = "10.10.10.1"
   vlan_id        = 10
   cores          = 2
-  memory         = 2048
+  memory         = 1024
   ssh_public_key = var.PUBLIC_SSH_KEY
 }
 
