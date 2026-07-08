7f77609939a3c0cd7bddc046d2482bb6493c5c71
Cyb3rCl0n3 <96691951+Cyb3rCl0n3@users.noreply.github.com>
2026-07-08 12:07:34 +0000

    test

--- proxmox/terraform/main.tf
+++ proxmox/terraform/main.tf
@@ -16,14 +16,14 @@ module "test_vm" {
   ssh_public_key = var.PUBLIC_SSH_KEY
 }
 
-# module "test_vm_2" {
-#   source         = "./modules/ubuntu-vm"
-#   vmid           = 9102
-#   hostname       = "test-02"
-#   ip_address     = "10.10.10.12/24"
-#   gateway        = "10.10.10.1"
-#   vlan_id        = 10
-#   cores          = 2
-#   memory         = 2048
-#   ssh_public_key = var.PUBLIC_SSH_KEY
-# }
+module "test_vm_2" {
+  source         = "./modules/ubuntu-vm"
+  vmid           = 9102
+  hostname       = "test-02"
+  ip_address     = "10.10.10.12/24"
+  gateway        = "10.10.10.1"
+  vlan_id        = 10
+  cores          = 2
+  memory         = 2048
+  ssh_public_key = var.PUBLIC_SSH_KEY
+}
