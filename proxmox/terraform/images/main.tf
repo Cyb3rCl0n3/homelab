f37f9660a28d384acee7bdd27b8706c6556562ad
Cyb3rCl0n3 <96691951+Cyb3rCl0n3@users.noreply.github.com>
2026-07-09 13:43:21 +0000

    test new workflow

--- proxmox/terraform/images/main.tf
+++ proxmox/terraform/images/main.tf
@@ -4,12 +4,14 @@ module "ubuntu_template" {
   name           = "ubuntu-2404-template"
   image_url      = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   image_filename = "noble-server-cloudimg-amd64.qcow2"
+  image_downloaded = true
 }
 
 module "debian_template" {
   source         = "../modules/vm-images"
-  vmid           = 9002
+  vmid           = 9001
   name           = "debian-12-template"
   image_url      = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2"
   image_filename = "debian-12-genericcloud-amd64.qcow2"
+  image_downloaded = true
 }
